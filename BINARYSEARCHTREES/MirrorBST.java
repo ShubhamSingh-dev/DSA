99a114fe257e293c8f0afbed18d43fa7301234d2
Shubham Singh <mine.shubhamsingh@gmail.com>
2025-03-19 22:33:59 +0530

    BST 7

---
+++ BINARYSEARCHTREES/MirrorBST.java
@@ -0,0 +1,13 @@
+public class MirrorBST {
+  static class Node {
+    int data;
+    Node left, right;
+    Node(int data) {
+      this.data = data;
+      this.left = this.right = null;
+    }
+  }
+  public static void main(String[] args) {
+    
+  }
+}

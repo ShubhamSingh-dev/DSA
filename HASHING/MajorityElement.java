c3ccd3adf6322f219bcfa3a916c4d397d46e05b9
Shubham Singh <mine.shubhamsingh@gmail.com>
2025-07-17 18:25:12 +0530

    Valid Anagram

---
+++ HASHING/MajorityElement.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class MajorityElement {
+  public static void main(String args[]) {
+    int arr[] = {1,3,2,5,1,3,1,5,1};
+    HashMap<Integer,Integer> map = new HashMap<>(); 
+
+    for(int i=0;i<arr.length;i++ ) {
+      // if(map.containsKey(arr[i])) {
+      //   map.put(arr[i], map.get(arr[i])+1);
+      // } else {
+      //   map.put(arr[i], 1);
+      // }
+
+      //the below code works same as above
+      map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+    }
+
+    // Set<Integer> keys = map.keySet();  // we dont have to same keyset in Set , we can directly access keyset of map in forloop like below
+    for(Integer key :  map.keySet()) {
+      if(map.get(key) > (arr.length/3)) {
+        System.out.println(key);
+      }
+    }
+  }
+}

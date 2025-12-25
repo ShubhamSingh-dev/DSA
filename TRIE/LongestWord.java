164cb7b46e521b34da5a0bf4353493e0a9d4e0af
Shubham Singh <mine.shubhamsingh@gmail.com>
2025-12-25 20:07:54 +0530

    Trie completed

---
+++ TRIE/LongestWord.java
@@ -0,0 +1,78 @@
+// Longest word with all prefixes present in array
+public class LongestWord {
+  static class Node {
+    Node[] children = new Node[26];
+    boolean eow;
+
+    public Node() {
+      for(int i = 0; i < 26; i++) {
+        children[i] = null;
+      }
+    
+    }
+  }
+
+  public static Node root = new Node();
+
+  public static void insert(String word) { //O(L) where L is the length of the largest word
+    int level=0;
+    int len = word.length();
+    int idx = 0;
+
+    Node curr = root;
+    for(level =0; level < len ; level++) {
+      idx = word.charAt(level) - 'a';
+      if(curr.children[idx] == null) {
+        curr.children[idx] = new Node();
+      }
+      curr = curr.children[idx];
+    }
+    curr.eow = true;
+  }
+
+  public static boolean search(String key) { //O(L)
+    int level = 0;
+    int len = key.length();
+    int idx = 0;
+
+    Node curr = root;
+    for(level = 0; level < len; level++) {
+      idx = key.charAt(level) - 'a';
+      if(curr.children[idx] == null) {
+        return false;
+      }
+      curr = curr.children[idx];
+    }
+    return curr.eow;
+  }
+
+  public static String ans = "";
+  public static void longestWord(Node root, StringBuilder temp) {
+    if(root == null){
+      return;
+    }
+
+    for(int i=0;i<26; i++) {
+      if(root.children[i] != null && root.children[i].eow == true){
+        char ch = (char)(i + 'a');
+        temp.append(ch);
+        if(temp.length() > ans.length()) {
+          ans = temp.toString();
+        }
+        longestWord(root.children[i], temp);
+        temp.deleteCharAt(temp.length() - 1); //backtracking  
+      }
+    }
+  }
+  
+  public static void main(String[] args) {
+    String words[] = {"a", "ap", "app", "appl", "apple", "apply"};
+
+    for(int i=0; i< words.length; i++) {
+      insert(words[i]);
+    }
+    longestWord(root, new StringBuilder(""));
+    System.out.println("Longest Word is: " + ans);
+    
+  }
+}
